dc9f239928981e908e1a464f7c8f7d6b02c5be5f
jbavari <josh@drifty.com>
2015-08-26 23:25:58 -0600

    Add compress images step

--- index.js
+++ index.js
@@ -1,58 +1,9 @@
 'use strict';
-var argv = require('yargs')
-	.usage('Usage: usemin [input.html] [--dest|-d dir] [--output|-o output.html] [options]')
-	.example('usemin src/index.html -d dist -o dist/index.html')
-	.example('usemin src/index.html -d dist > dist/index.html')
-	.options({
-		'd': {
-			alias: 'dest',
-			demand: 'Please specify the output directory',
-			describe: 'Output directory for compressed output files',
-			type: 'string'
-		},
-		'o': {
-			alias: 'output',
-			describe: 'HTML output file',
-			type: 'string'
-		},
-		'htmlmin': {
-			default: false,
-			describe: 'Also minifies the input HTML file',
-			type: 'boolean'
-		},
-		'rmlr': {
-			alias: 'removeLivereload',
-			default: false,
-			describe: 'Remove livereload script',
-			type: 'boolean'
-		}
-	})
-	.demand(1)
-	.argv;
 
-var fs = require('fs');
-var getBlocks = require('./lib/getBlocks');
-var processBlocks = require('./lib/processBlocks');
-var getHTML = require('./lib/getHTML');
+var fs = require('fs'),
+    Usemin = module.exports;
 
-var filePath = argv._[0];
-var content = fs.readFileSync(filePath).toString();
-var blocks = getBlocks(argv._[0], content, argv.removeLivereload);
-var process = processBlocks(blocks, argv.dest);
-var output = getHTML(content, blocks, argv.htmlmin);
-
-if (process) {
-	if (argv.o) {
-		fs.writeFile(argv.o, output, function(err) {
-			if (err) {
-				throw Error(err);
-			}
-		});
-	}
-	else {
-		console.log(output);
-	}
-}
-else {
-	throw Error('Unexpected error.');
-}
+Usemin.getBlocks = require('./lib/getBlocks');
+Usemin.processBlocks = require('./lib/processBlocks');
+Usemin.getHTML = require('./lib/getHTML'),
+Usemin.compressImages = require('./lib/compressImages');
